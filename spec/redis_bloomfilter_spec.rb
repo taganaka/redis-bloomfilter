89cbf9a3cfad351112b8c0aaa4aa6ad40eebc1e7
Francesco Laurita <taganaka@gmail.com>
2013-06-02 15:50:58 -0700

    Test for multi driver

--- spec/redis_bloomfilter_spec.rb
+++ spec/redis_bloomfilter_spec.rb
@@ -14,7 +14,13 @@ def test_error_rate(bf,elems)
   error.to_f / elems
 end
 
+def factory options, driver
+  options[:driver] = driver
+  Redis::Bloomfilter.new options
+end
+
 describe Redis::Bloomfilter do
+
   it 'should return the right version' do
     Redis::Bloomfilter.version.should eq "redis-bloomfilter version #{Redis::Bloomfilter::VERSION}"
   end
@@ -27,30 +33,33 @@ describe Redis::Bloomfilter do
   end
 
   it 'should create a Redis::Bloomfilter object' do
-    bf = Redis::Bloomfilter.new :size => 1000, :error_rate => 0.01, :key_name => 'ossom'
+    bf = factory({:size => 1000, :error_rate => 0.01, :key_name => 'ossom'}, 'ruby')
     bf.should be
     bf.options[:size].should eq 1000
     bf.options[:bits].should eq 9585
     bf.options[:hashes].should eq 6
     bf.options[:key_name].should eq 'ossom'
+    bf.clear
   end
+  
+  %w(ruby lua).each do |driver|
+    it 'should work' do
+      bf = factory({:size => 1000, :error_rate => 0.01, :key_name => '__test_bf'},driver)
+      bf.clear
+      bf.include?("asdlol").should be false
+      bf.insert "asdlol"
+      bf.include?("asdlol").should be true
+      bf.clear
+      bf.include?("asdlol").should be false
+    end
 
-  it 'should work' do
-    bf = Redis::Bloomfilter.new :size => 1000, :error_rate => 0.01, :key_name => '__test_bf'
-    bf.clear
-    bf.include?("asdlol").should be false
-    bf.insert "asdlol"
-    bf.include?("asdlol").should be true
-    bf.clear
-    bf.include?("asdlol").should be false
-  end
-
-  it 'should honor the eror rate' do
-    bf = Redis::Bloomfilter.new :size => 100, :error_rate => 0.01, :key_name => '__test_bf'
-    bf.clear
-    e = test_error_rate bf, 180
-    e.should be < bf.options[:error_rate]
-    bf.clear
+    it 'should honor the eror rate' do
+      bf = factory({:size => 100, :error_rate => 0.01, :key_name => '__test_bf'},driver)
+      bf.clear
+      e = test_error_rate bf, 180
+      e.should be < bf.options[:error_rate]
+      bf.clear
+    end
   end
 
 end
