d8cf15e54600b4e4863c4cd5a0b5475a041b3dad
Francesco Laurita <taganaka@gmail.com>
2013-06-02 19:01:50 -0700

    Adding a flat benchmark script

---
+++ benchmark/bf_100_000_flat.rb
@@ -0,0 +1,23 @@
+$:.push File.expand_path("../lib", __FILE__)
+require "redis-bloomfilter"
+require "benchmark"
+items = 100_000
+error_rate = 0.01
+%w(lua ruby).each do |driver|
+  bf = Redis::Bloomfilter.new(
+    {
+      :size       => items, 
+      :error_rate => error_rate, 
+      :key_name   => "bloom-filter-bench-flat-#{driver}",
+      :driver     => driver
+    }
+  )
+  bf.clear
+  puts "---------------------------------------------"
+  puts "Benchmarking #{driver} driver with #{items} items"
+  Benchmark.bm(7) do |x|
+    x.report("insert:  ") {items.times { |i| bf.insert(rand(items)) }}
+    x.report("include?:") {items.times { |i| bf.include?(rand(items)) }}
+  end
+  puts
+end
