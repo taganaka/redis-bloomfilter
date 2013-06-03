5344bdf590d8ddbecddd4d4830dd84336312790b
Francesco Laurita <taganaka@gmail.com>
2013-06-02 17:41:03 -0700

    This driver is much more faster

--- lib/bloomfilter_driver/ruby.rb
+++ lib/bloomfilter_driver/ruby.rb
@@ -1,11 +1,11 @@
-require "digest/md5"
 require "digest/sha1"
-require "zlib"
 class Redis
   module BloomfilterDriver
     class Ruby
+      
+      # Faster Ruby version.
+      # This driver should be used if Redis version < 2.6
       attr_accessor :redis
-
       def initialize(options = {})
         @options = options
       end
@@ -13,15 +13,15 @@ class Redis
       # Insert a new element
       def insert(data) 
         @redis.pipelined do
-          indexes_for(data) { |i| @redis.setbit @options[:key_name], i, 1 }
+          indexes_for(data).each {|i| @redis.setbit @options[:key_name], i, 1}
         end
       end
 
       # It checks if a key is part of the set
       def include?(key)
-        indexes = []
-        indexes_for(key) { |idx| indexes << idx }
 
+        indexes = []
+        indexes_for(key).each { |idx| indexes << idx }
         return false if @redis.getbit(@options[:key_name], indexes.shift) == 0
 
         result = @redis.pipelined do
@@ -37,24 +37,22 @@ class Redis
       end
 
       protected
-        def indexes_for(key, engine = nil)
-          engine ||= @options[:hash_engine]
-          @options[:hashes].times do |i|
-            yield self.send("engine_#{engine}", key.to_s, i)
-          end
-        end
+        # Hashing strategy: 
+        # http://www.eecs.harvard.edu/~kirsch/pubs/bbbf/esa06.pdf
+        def indexes_for data
+          sha = Digest::SHA1.hexdigest(data.to_s)
+          h = []
+          h[0] = sha[0..8].to_i(16)
+          h[1] = sha[8..16].to_i(16)
+          h[2] = sha[16..24].to_i(16)
+          h[3] = sha[24..32].to_i(16)
+          idxs = []
 
-        # A set of different hash functions
-        def engine_crc32(data, i)
-          Zlib.crc32("#{i}-#{data}").to_i(16) % @options[:bits]
-        end
-
-        def engine_md5(data, i)
-          Digest::MD5.hexdigest("#{i}-#{data}").to_i(16) % @options[:bits]
-        end
-
-        def engine_sha1(data, i)
-          Digest::SHA1.hexdigest("#{i}-#{data}").to_i(16) % @options[:bits]
+          (@options[:hashes]).times {|i|
+            v = (h[i % 2] + i * h[2 + (((i + (i % 2)) % 4) / 2)]) % @options[:bits]
+            idxs << v
+          }
+          idxs
         end
     end
   end
